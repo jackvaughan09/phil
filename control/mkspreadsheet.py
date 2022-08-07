4e22db801e7aa44674a0b88aedd6314d7b4e4e10
Hudson Nash <hudsonnash@Hudsons-MacBook-Air-2.local>
2022-08-07 01:25:03 -0500

    The next issue to iron out is the error from tabula package: FileNotFoundError: [Errno 2] No such file or directory: '/Users/hudsonnash/Dropbox/Mac/Desktop/Su22s/phil/phil/control/02-LamitanCity2010_Status_of_Prior_Years_Audit_Recommendations.pdf'

--- control/mkspreadsheet.py
+++ control/mkspreadsheet.py
@@ -10,14 +10,12 @@ Created on Wed Jun 22 12:25:52 2022
 
 import os
 import pandas as pd
+import tabula as t
 def table_from_txt(udir):
-    frame = pd.DataFrame()
-    for f in os.listdir(udir):
-        if f[f.find('.')+1:] == 'txt':
-            temp = pd.read_csv(str(os.path.join(udir,f)),sep='|')
-            frame = temp
-        #TODO: do something with temp to append it to returned frame (if exists)     
-    return frame
+    for file in os.listdir(udir):
+        if os.path.splitext(file)[1] == '.pdf':
+            table = t.read_pdf(os.path.abspath(file))
+            #TODO: This generates an error (FILE CANNOT BE FOUND ERROR)
+    return table
 if __name__ == '__main__':
     df = table_from_txt(os.path.join(os.path.dirname(os.getcwd()),'data/unzipped'))
-    df.to_excel(os.path.abspath('../data/xlsx/most_recent_output.xlsx'))
