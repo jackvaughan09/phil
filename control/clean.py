4aaed0ffc6ffdfe6d35c7cc507a0b06e3d4f9d73
Hudson Nash <hudsonnash@hudsons-air-2.lan>
2022-09-02 11:54:07 -0500

    Tinkering with Garden City folder and trying to get a generalized extraction procedure to work.

--- control/clean.py
+++ control/clean.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-CLoEAN_DUMP_TO_DIR.PY
+CLEAN.PY
 
 Created on Thurs June 23, 2022 at 5:50 PM
 
@@ -15,6 +15,8 @@ import subprocess
 import PyPDF2 as p
 import config as c
 import mksheet as s
+import pandas as pd
+from difflib import SequenceMatcher
 def unzip(diz,diu):
     z_list = [os.path.join(diz,f) for f in os.listdir(diz) if os.path.isfile(os.path.join(diz,f)) and f[0] != '.']
     for z in z_list:
@@ -22,36 +24,52 @@ def unzip(diz,diu):
             src.extractall(diu)
 def keep_only_pages_after(filename,start_page):
     infile = p.PdfReader(filename, 'rb')
+    print(start_page)
     pages_to_keep = range(start_page, infile.numPages) # page numbering starts from 0
     output = p.PdfWriter()
     for i in pages_to_keep:
         page = infile.pages[i] 
         output.add_page(page)
+    print(output)
     with open(filename, 'wb') as f:
         output.write(f)
-def default_table_search(di):
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+def col_fn(fn,df):
+    df['Filename']=[fn for row in range(len(df))]
+    return df[['Filename']+[col for col in df if col not in ['Filename']]]
+def extract(di):
     # search for the correct page and file to begin extraction
     target_page = -1
     table_end_page = -1
+    out = pd.DataFrame()
     for filename in os.listdir(di):
         if os.path.splitext(filename)[1] == ".pdf":
-            print(filename)
             filename = os.path.join(di,filename)
             pdf = p.PdfFileReader(filename)
             page_count = pdf.numPages
-            print(page_count)
             for pagenum in range(page_count):
                 page = pdf.getPage(pagenum)
                 page_content = page.extractText()
                 # print(page_content.lower())
                 if all([i in page_content.lower() for i in c.TARGET_SENTENCE]):
-                    print('true')
                     target_page = pagenum
                     keep_only_pages_after(filename,pagenum)
-                    df = s.default(filename) # feed chucks of PDF
-                    return df
-                # TODO: MANUAL MERGE COLUMNS
-                                 
+                    dfs = s.default(filename) # feed chunks of PDF
+                    if len(dfs) > 1:
+                        for df in dfs:
+                            df = col_fn(filename,df)
+                        df = manual_merge(dfs)
+                        out = out.append(df,ignore_index=True)
+                    elif len(dfs) == 1:
+                        df = col_fn(filename,dfs[0])
+                        out = out.append(df)
+    print(out)
+    return out
+                    
+def manual_merge(dfs):
+    for df in dfs:
+        print(df)                     
 def rem_unwanted_data(di):
     # remove all of the files in ../data/unzipped not necessary for mkspreadsheet.py
     # depending on audit format
@@ -77,7 +95,7 @@ if __name__ == '__main__':
     unzipped_dir = os.path.join(_dir,'data','unzipped')
     unzip(zipped_dir,unzipped_dir)
     rem_unwanted_data(unzipped_dir)
-    page_content = default_table_search(unzipped_dir)
+    # page_content = default_table_search(unzipped_dir)
     # running the convert.sh bash script doesn't work when done in Python:
     # subprocess.Popen('bash convert.sh ../data/unzipped',shell=True)
     # therefore, it should be run separately
--- control/mksheet.py
+++ control/mksheet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-MKSPREADSHEET.PY
+MKSHEET.PY
 
 Created on Wed Jun 22 12:25:52 2022
 
@@ -11,10 +11,11 @@ import os
 import tabula.io as tab
 import re
 import pandas as pd
+import clean as cl
+import argparse
 
 def default(filename):
     dfs = tab.read_pdf(filename,lattice=True,pages='all') # pages attribute of tabula-py is broken
-    print(type(dfs))
     return dfs
 def lamitan(udir):
     for file in os.listdir(udir):
@@ -22,7 +23,11 @@ def lamitan(udir):
             dfs = tab.read_pdf(os.path.join(udir,file),lattice=True,pages='all')
             return pd.concat(dfs)
 if __name__ == '__main__':
-    # TODO : Classify the document as Lamitan format or Garden City format
-    df = lamitan(os.path.join(os.path.dirname(os.getcwd()),'data/unzipped'))
-    df.to_excel('test.xlsx')
-    
+    # df = lamitan(os.path.join(os.path.dirname(os.getcwd()),'data/unzipped'))
+    # df.to_excel('test.xlsx')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d")
+    args = parser.parse_args()
+    df = cl.extract(args.d)
+    print(df)
+    df.to_excel('../data/xlsx/test.xlsx')
