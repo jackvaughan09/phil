5936e0d090edfc95245a116167fd7b8bddb31336
Hudson Nash <hudsonnash@Hudsons-MacBook-Air-2.local>
2022-09-20 04:03:55 -0500

    Fixed a little bug in clean.keep_only_pages_after

---
+++ test/pagetest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3A
+# -*- coding: utf-8 -*-
+"""
+PAGETEST.PY
+
+@author: hudsonnash
+"""
+import sys, os
+sys.path.append('../control/')
+import clean as cl
+print(cl.keep_only_pages_after('../test/test.pdf',3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
