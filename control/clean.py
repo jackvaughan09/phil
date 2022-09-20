771396a7f9b63150d31f40961fd64f448aa241f3
Hudson Nash <hudsonnash@Hudsons-MacBook-Air-2.local>
2022-09-20 05:33:29 -0500

    Fixed long-standing error and tried to add more user-friendly messages.

--- control/clean.py
+++ control/clean.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3A
 # -*- coding: utf-8 -*-
 """
 CLEAN.PY
@@ -24,52 +24,76 @@ def unzip(diz,diu):
             src.extractall(diu)
 def keep_only_pages_after(filename,start_page):
     infile = p.PdfReader(filename, 'rb')
-    print(start_page)
     pages_to_keep = range(start_page, infile.numPages) # page numbering starts from 0
     output = p.PdfWriter()
     for i in pages_to_keep:
         page = infile.pages[i] 
         output.add_page(page)
-    print(output)
-    with open(filename, 'wb') as f:
-        output.write(f)
+        with open(filename, 'wb') as f:
+            output.write(f)
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 def col_fn(fn,df):
     df['Filename']=[fn for row in range(len(df))]
     return df[['Filename']+[col for col in df if col not in ['Filename']]]
+def replace_col(col_name_new,col_name_old):
+    #TODO
+    pass
 def extract(di):
     # search for the correct page and file to begin extraction
     target_page = -1
     table_end_page = -1
     out = pd.DataFrame()
+    file_counter = 1
     for filename in os.listdir(di):
+        print('Looking for file n = '+str(file_counter)+'...')
         if os.path.splitext(filename)[1] == ".pdf":
             filename = os.path.join(di,filename)
+            print('Now scanning '+filename)
             pdf = p.PdfFileReader(filename)
             page_count = pdf.numPages
-            for pagenum in range(page_count):
+            only_one_df = True
+            no_df_found = True
+            for pagenum in range(page_count-1):
+                print('Scanning '+str(pagenum+1)+' of '+str(page_count)+' pages')
                 page = pdf.getPage(pagenum)
                 page_content = page.extractText()
+                #print(page_content)
                 # print(page_content.lower())
                 if all([i in page_content.lower() for i in c.TARGET_SENTENCE]):
+                    no_df_found = False
                     target_page = pagenum
-                    keep_only_pages_after(filename,pagenum)
+                    keep_only_pages_after(filename,target_page)
                     dfs = s.default(filename) # feed chunks of PDF
                     if len(dfs) > 1:
                         for df in dfs:
-                            df = col_fn(filename,df)
-                        df = manual_merge(dfs)
-                        out = out.append(df,ignore_index=True)
-                    elif len(dfs) == 1:
+                            df = col_fn(filename,df) # rename columns
+                            print('One dataframe added of multiple')
+                            print(df['Audit Observation'])
+                            out = out.append(df,ignore_index=True)
+                            print('Output in main little loop:')
+                            print(out['Audit Observation'])
+                            only_one_df = False
+                        #df = manual_merge(dfs) #TODO)
+                    elif len(dfs) == 1 and only_one_df:
                         df = col_fn(filename,dfs[0])
                         out = out.append(df)
-    print(out)
+                        print('One dataframe added')
+                        print(df['Audit Observation'])
+                        print('Single DF in file')
+        if no_df_found:
+            error('Could not locate tables in file n = '+file_counter+' -- adjust target sequences list in config.py')
+        file_counter = file_counter + 1
+        print('Final output:')
+    print(out['Audit Observation'])
     return out
                     
 def manual_merge(dfs):
+    cols = 0 #TODO
     for df in dfs:
-        print(df)                     
+        if this == this:
+            that = 0
+    #TODO
 def rem_unwanted_data(di):
     # remove all of the files in ../data/unzipped not necessary for mkspreadsheet.py
     # depending on audit format
