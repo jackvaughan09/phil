d9b200aee622ac7fca5e2a0ac6b615b89ceca0fc
J. Hudson Nash <35884292+jhudsonnash@users.noreply.github.com>
2022-12-17 14:46:44 -0600

    added to requirements.txt

--- control/convert.sh
+++ control/convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for file in ($1).{doc,docx} ; do
+for file in "$1"/{*.doc,*.docx} ; do
     # antiword "$file" > "${file%.doc}.pdf"
     # for some reason (and I'm inexperienced in bash), calling on antiword first helps unoconv...
     $2 $3 "$file";
