7dd835663cd651880e01ddd876331f9ecf52c122
Jack Vaughan <jackvaughan09@gmail.com>
2023-01-15 12:42:59 -0600

    Added helper funcs, standardizes output

--- control/extract_all.py
+++ control/extract_all.py
@@ -1,10 +1,41 @@
 from extract import extract
 import os
 import pandas as pd
-from config import CANON_HEADERS
+from config import CANON_HEADERS, AUTOCORRECT_DICT
+
+
+def assert_canon_columns(dfs):
+    for i, df in enumerate(dfs):
+        # Implement autocorrect for column names
+        for k,v in AUTOCORRECT_DICT.items():
+            if k in df.columns:
+                dfs[i] = dfs[i].rename(columns={k:v})
+        # find difference between df and canon headers
+        diff = set(CANON_HEADERS)-set(df.columns) 
+        if diff:
+            # create the canon column and fill with blank strings
+            for label in diff:
+                dfs[i][label] = ''
+    return dfs
+
+def remove_body_header_rows(df):
+    df = df.loc[
+        (df['status of implementation'] != 'Status ofImplementation') &
+        (df['management action'] != 'ManagementAction') &
+        (df['recommendations'] != 'Recommendations')
+    ]
+    return df
+
 def extract_all(pdf_dir) -> pd.DataFrame:
-    df = pd.DataFrame(columns=CANON_HEADERS)
+    dfs = []
     for fi in os.listdir(pdf_dir):
         if os.path.splitext(fi)[1] == '.pdf':
-            df = pd.concat([df,extract(os.path.join(pdf_dir,fi))], ignore_index = True)
-    return df
+            dfs.append(extract(os.path.join(pdf_dir,fi)))
+    # remove NoneType values
+    dfs = list(filter(lambda x: x is not None,dfs))
+    # then assert equal columns
+    dfs = assert_canon_columns(dfs)
+    # concat result
+    full_df = pd.concat(dfs)
+    full_df = remove_body_header_rows(full_df)
+    return full_df
