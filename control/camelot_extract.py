40a5ccc03edbbf69891ae2a8dd1b1c8ac462d3f1
Jack Vaughan <jackvaughan09@gmail.com>
2023-01-20 08:18:39 -0600

    Refactor and update

--- control/camelot_extract.py
+++ control/camelot_extract.py
@@ -1,70 +1,44 @@
+'''
+This file presents an alternate PDF Reader Library -- Camelot
+as an optimal solution for scraping PDFs of non-specific
+file formats (those that do not match Adobe's standards)
+
+Author: @jackvaughan09
+'''
+
 import pandas as pd
-from extracttools import good_match
+from extracttools import get_pg_rng, year, city
+from camelot_tools import locate_relevant_camelot_tables
+from philformat import phil_format, overflow_repair
 from config import CANON_HEADERS
 import camelot
 import code
 
-def locate_relevant_camelot_tables(dfs):
-    print("Attempting to locate relevant tables...")
-    out = []  
-    counter = 0                          
-    for i, df in enumerate(dfs):
-        print('Cleaning headers...', end=' ')
-        # camelot shoves the column headers into the first row of the df
-        # clean and store values
-        df.iloc[0]= df.iloc[0].apply(lambda x: x.replace('\n',''))
-        columns = df.iloc[0]
-        columns = [good_match(col,CANON_HEADERS) for col in columns]
-        print('Done!')
-        # fuzzy logic check if first row is good match of target headers
-        # if no, remove df from dfs
-        print('Checking for relevance...', end=' ')
-        try:
-            if len(set(CANON_HEADERS).intersection(set(columns))) <= 4:
-                counter += 1
-                print(f'Irrelevant, removed table {i+1}/{len(dfs)}')
-                continue
-        except Exception as e:
-            print(f"Error: {e}")
-        
-        print('Relevant table collected!')
-        # if yes, then make the first row the headers of the new df
-        df.columns = columns
-        df = df.iloc[1:] 
-        out.append(df.astype(str))
-    if len(out)<1:
-        return pd.DataFrame(columns=CANON_HEADERS)
-
-    print(f'Collected {len(out)} tables!\n'
-          f'Removed {counter}')
-    return out
-
-def camelot_polish(df):
-    return df.replace(r'\n','',regex=True)
-
-def camelot_overflow_repair(df):
-    overflow_rows = [i for i,x in enumerate(df['']) if x == '']
-    for i in overflow_rows:
-        df.loc[i-1]+=df.loc[i]
-    df = df.drop(index=overflow_rows).reset_index(drop=True)
-    return df
-
-def camelot_extract(pdf_url, pg_rng):
-    print('Attempting alternate file reader,'
-          '\nThis might take a while.')     # <-- valid syntax
+def camelot_extract(pdf_url):
+    pg_rng = get_pg_rng(pdf_url) #TODO: improving pg_rng function accuracy:
+    if pg_rng == '0':
+        return
+    print(f'Attempting alternate file reader for {pdf_url}'
+          '\nThis might take a while.')    
     
-    # ensure string typing, keeps code from breaking later on
+    # ensure string typing
     dfs = [df.df.astype(str) for df in camelot.read_pdf(
-        filepath = pdf_url,
-        pages = pg_rng,                 #TODO: improving pg_rng function accuracy:
+        filepath = pdf_url,                
         flavor = 'lattice',
-        multiple_tables = True,                   
+        multiple_tables = True,
+        copy_text = ['v'], 
+        line_scale = 30,
+        pages = pg_rng
     )]                                  
-    print('Finished reading all tables from file')
+    print(f'Finished reading {len(dfs)} tables from file {pdf_url}')
+    if len(dfs)< 1:
+        print('No tables found. Continuing.')
+        return pd.DataFrame(columns=CANON_HEADERS)
+    
     dfs = locate_relevant_camelot_tables(dfs)
-    dfs = [camelot_polish(df) for df in dfs]
-    out = pd.concat(dfs,ignore_index=True,axis=0)
-    out = camelot_overflow_repair(out)
+    dfs = phil_format(dfs, pdf_url)
+    out = pd.concat(dfs).reset_index(drop=True)
+    out = overflow_repair(out)
     return out
 
 
