6a7196e4a006ce100a9cde4f08f7e814d1a7ac2d
Jack Vaughan <jackvaughan09@gmail.com>
2023-01-27 19:55:49 -0600

    Autocheck/build data/zip and /pdf

--- phil.sh
+++ phil.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#creates these folders if they don't just in case.
+if [ ! -d "data/pdf" ]; then
+  mkdir -p "data/pdf"
+fi
+
+if [ ! -d "data/zip" ]; then
+  mkdir -p "data/zip"
+fi
 docker build -t phil .
 docker run --name philapp phil
 docker cp philapp:app/data extracted
