f39181a8ea552d018a7ced295a0514d5130493bf
Jack Vaughan <jackvaughan09@gmail.com>
2023-01-29 16:17:55 -0600

    Much smarter now, duplicate protection

--- control/convert.sh
+++ control/convert.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/sh
 for f in "$1"/*; do
+  filename="$(basename "$f")"
+  pdf_file="$(echo "$filename" | sed 's/\.[^.]*$/.pdf/')"
+  if [ -f "../data/pdf/$pdf_file" ]; then
+    echo "PDF version already exists: $pdf_file"
+    continue
+  fi
 	if [ "$(head -c 4 "$f")" = "%PDF" ]; then
-		echo "PDF detected, continuing"
-		mv "$f" ../data/pdf
+		echo "PDF detected, moving to pdf directory"
+		mv "$f" "../data/pdf/$pdf_file"
 		continue
 	fi
 	echo "Converting: $f"
