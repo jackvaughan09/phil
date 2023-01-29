8b3c9f1cb6f0cac4dbf32d38ca448ec59f504335
Jack Vaughan <jackvaughan09@gmail.com>
2023-01-29 16:13:50 -0600

    Separate from win version

--- phil.sh
+++ phil.sh
@@ -3,13 +3,12 @@
 if [ ! -d "data/pdf" ]; then
   mkdir -p "data/pdf"
 fi
-
 if [ ! -d "data/zip" ]; then
   echo "Please create a folder called 'zip' in /data and add some zip files!"
   exit
 fi
-docker build -t phil .
-docker run --name philapp phil
+docker build -t phil:local -f Dockerfile .
+docker run --name philapp phil:local
 docker cp philapp:app/data/xlsx extracted
 docker container rm -f philapp
-docker image rm -f phil
+docker image rm -f phil:local
