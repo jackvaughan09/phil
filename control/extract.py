d32743a56c759193deb38b4723726cc3b83279aa
Jack Vaughan <jackvaughan09@gmail.com>
2023-03-27 16:59:52 -0500

    V2

--- control/extract.py
+++ control/extract.py
@@ -5,14 +5,14 @@ Author: @jackvaughan09
 import camelot
 import pandas as pd
 from config import CANON_HEADERS
-from extracttools import locate_relevant_tables, get_pg_rng
+from extracttools import header_match_tables, find_part3_rng
 from philformat import overflow_repair, phil_format
 
 
 def extract(pdf_url):
-    pg_rng = get_pg_rng(pdf_url)
+    pg_rng = find_part3_rng(pdf_url)
     if len(pg_rng) == 0:
-        print("No tables found. Continuing.")
+        print("No Part III found. Continuing.")
         return pd.DataFrame(columns=CANON_HEADERS)
 
     print(f"Attempting to read tables from: {pdf_url}" "\nThis might take a while.")
@@ -22,7 +22,7 @@ def extract(pdf_url):
             filepath=pdf_url,
             flavor="lattice",
             multiple_tables=True,
-            copy_text=["v"],
+            # copy_text=["v"],
             line_scale=30,
             pages=pg_rng,
         )
@@ -31,12 +31,13 @@ def extract(pdf_url):
         print("No tables found. Continuing.")
         return pd.DataFrame(columns=CANON_HEADERS)
     print(f"Finished reading {len(dfs)} tables from file {pdf_url}")
-    dfs = locate_relevant_tables(dfs)
-    if type(dfs) == pd.DataFrame:
-        return dfs
-
+    dfs = header_match_tables(dfs)
     dfs = phil_format(dfs, pdf_url)
-    out = pd.concat(dfs).reset_index(drop=True)
+    try:
+        out = pd.concat(dfs).reset_index(drop=True)
+    except Exception as e:
+        print(f"Error: {e}")
+        return pd.DataFrame(columns=CANON_HEADERS)
     # one more for good measure.
-    out = overflow_repair(out)
+    # out = overflow_repair(out)
     return out
